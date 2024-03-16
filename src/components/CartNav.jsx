2b2dafaf62074e12f961d58f50e17b923a2ab1a3
EsakkiRajM <esakkiraj1150@gmail.com>
2024-03-16 19:39:01 +0530

    Add the image in cards

--- src/components/CartNav.jsx
+++ src/components/CartNav.jsx
@@ -5,7 +5,7 @@ const CartNav = () => {
 
     const dataCards = {
         eachCards: [{
-            imgSrc: "./src/assets/ProductImages/bingo.webp",
+            imgSrc: "../src/assets/ProductImages/bingo.webp",
             pdtName: "Bingo",
             price: "$40.00"
         }]
@@ -14,7 +14,7 @@ const CartNav = () => {
     return (
         <div><nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container px-4 px-lg-5">
-                <a className="navbar-brand" href="#!">Start Bootstrap</a>
+                <a className="navbar-brand" href="#!">ES Hyper Market</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
